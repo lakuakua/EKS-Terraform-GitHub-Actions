8b3747d6f725a98d518f4912c7c64c38e1ff901d
akshay751989 <69006696+akshay751989@users.noreply.github.com>
2024-09-22 15:45:11 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "my-aws-backet-akshays"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
