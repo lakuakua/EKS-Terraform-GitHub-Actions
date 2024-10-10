e7f2cd767cbd140c6da84ce96b4ef0e8af95b2bb
lakuakua <60492957+lakuakua@users.noreply.github.com>
2024-10-10 14:36:55 -0500

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,4 +1,4 @@
-terraform {
+stterraform {
   required_version = "~> 1.9.3"
   required_providers {
     aws = {
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "my-ews-baket1980"
-    region         = "us-west-1"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
      # dynamodb_table = "Lock-Files"
     encrypt        = true
